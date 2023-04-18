ddc002981a6373c15a5c14e58e01f4520f3f5d2a
Halina Pishchala <halina_pishchala@epam.com>
2023-04-19 00:03:25 +0300

    feat: parsed csv file with prouct data

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -1,19 +1,57 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { headers } from '../constants';
+import { S3Event } from 'aws-lambda';
+import { REGION, headers } from '../constants';
 import { errorResponse } from '../errors-hadler';
-import { logRequestContextMessage } from '../logger';
+import { S3 } from 'aws-sdk';
+import csv from 'csv-parser';
 
-export const importFileParser = async (event) => {
-  //logRequestContextMessage(event.requestContext, fileName);
+export const importFileParser = async (event: S3Event) => {
+  const { Records } = event;
+
+  const s3Bucket = Records[0].s3.bucket.name;
+  const csvDataFile = Records[0].s3.object.key;
+
+  console.log('file:', csvDataFile, ' will be parsed in a bucket:', s3Bucket);
+
+  const s3 = new S3({ region: REGION });
+
+  const params = {
+    Bucket: s3Bucket,
+    Key: csvDataFile
+  };
 
   try {
+    await s3
+      .getObject(params)
+      .createReadStream()
+      .pipe(csv({ separator: ';' }))
+      .on('data', (data) => {
+        console.log('data:', data);
+      })
+      .on('end', async () => {
+        console.log('Csv file was parsed');
+
+        await s3
+          .copyObject({
+            Bucket: s3Bucket,
+            CopySource: s3Bucket + '/' + csvDataFile,
+            Key: csvDataFile.replace('uploaded', 'parsed')
+          })
+          .promise();
+
+        await s3
+          .deleteObject({
+            Bucket: s3Bucket,
+            Key: csvDataFile
+          })
+          .promise();
+      });
 
     return {
       statusCode: 200,
       headers,
       body: JSON.stringify(
         {
-          data: "some"
+          data: 'CSV file was parsed successfully'
         },
         null,
         2
